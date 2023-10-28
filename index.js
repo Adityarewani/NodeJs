4d43cc9a602a98540e33c7bc373705211e044ae2
Text Anlyzer <105407205+Adityarewani@users.noreply.github.com>
2023-10-28 07:51:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const http = require('http');
 var fs = require("fs");
+
+const server = http.createServer((req, res) => { Pres.writeHead (200, "Content-Type": "text/plain" });
+  res.end("Hello world!");
+});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("Server is running on port 3000"));
+
 // Defining new user 
 var data = fs.readFileSync('input.txt');
 fs.writeFileSync('input.txt', 'Hi');
